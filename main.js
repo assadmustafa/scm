ea6e21cfb2a7251e5bf85fbf71814bfc35b5cd6b
Assad Mustafa <sig.bonotti@gmail.com>
2024-04-16 13:12:48 +0200

    change style and new functions for settings buttons

--- main.js
+++ main.js
@@ -2,9 +2,74 @@ const mainContent = document.getElementById("mainContent");
 const pages = document.querySelectorAll(".page");
 const list = document.getElementById("current-pages");
 const savedCameras = JSON.parse(localStorage.getItem("cameras")) || [];
+var elem = document.body;
+
+function requestFullScreen(element) {
+  if (
+    !document.fullscreenElement && // alternative standard method
+    !document.mozFullScreenElement &&
+    !document.webkitFullscreenElement &&
+    !document.msFullscreenElement
+  ) {
+    // current working methods
+    if (elem.requestFullscreen) {
+      elem.requestFullscreen();
+    } else if (elem.msRequestFullscreen) {
+      elem.msRequestFullscreen();
+    } else if (elem.mozRequestFullScreen) {
+      elem.mozRequestFullScreen();
+    } else if (elem.webkitRequestFullscreen) {
+      elem.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+    }
+  } else {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.msExitFullscreen) {
+      document.msExitFullscreen();
+    } else if (document.mozCancelFullScreen) {
+      document.mozCancelFullScreen();
+    } else if (document.webkitExitFullscreen) {
+      document.webkitExitFullscreen();
+    }
+  }
+}
+
+function toggleFullScreenButton() {
+  requestFullScreen(elem);
+}
+
+// Clear Function
+function deleteAllCameras() {
+  const cameras = document.querySelectorAll(".camera");
+  cameras.forEach((camera) => {
+    camera.remove();
+  });
+
+  const listItems = document.querySelectorAll("#current-pages li");
+  listItems.forEach((item) => {
+    item.remove();
+  });
+
+  // Clear the localStorage as well
+  localStorage.removeItem("cameras");
+}
+
+// Exit Function
+function turnOff() {
+  // Display a confirmation dialog
+  var confirmed = confirm("Are you sure you want to exit?");
+
+  // Check if the user confirmed
+  if (confirmed) {
+      // Close the window
+      window.close();
+  }
+}
 
 function saveCamerasToLocalStorage() {
-  const cameras = Array.from(mainContent.querySelectorAll(".camera")).map(page => page.textContent);
+  const cameras = Array.from(mainContent.querySelectorAll(".camera")).map(
+    (page) => page.textContent
+  );
   localStorage.setItem("cameras", JSON.stringify(cameras));
 }
 
@@ -40,7 +105,7 @@ function drop(event) {
   btn.style = "color: red; margin-left:10px;";
   btn.style.cursor = "pointer";
   name.className = "name";
-  foto.className = "foto"
+  foto.className = "foto";
 
   item.textContent = name.innerText = pageContent;
 
@@ -60,7 +125,7 @@ function drop(event) {
 }
 
 // Restore previous session cameras on page load
-savedCameras.forEach(camera => {
+savedCameras.forEach((camera) => {
   const page = document.createElement("div");
   const name = document.createElement("div");
   const icon = document.createElement("i");
@@ -72,7 +137,7 @@ savedCameras.forEach(camera => {
   icon.className = "fa-solid fa-video green";
   btn.className = "fa-solid fa-trash btn";
   foto.src = "/images/screen.png";
-  foto.className = "foto"
+  foto.className = "foto";
   name.className = "name";
   item.textContent = name.innerText = camera;
 
