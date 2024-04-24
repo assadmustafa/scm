8c005573cb738a9bf4c31f13f7d96da4a0c038d5
Assad Mustafa <sig.bonotti@gmail.com>
2024-04-24 12:43:00 +0200

    add buttons for opened windows

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 const mainContent = document.getElementById("mainContent");
+const dropZone = document.getElementById("dropZone");
 const pages = document.querySelectorAll(".page");
 const list = document.getElementById("current-pages");
 const savedCameras = JSON.parse(localStorage.getItem("cameras")) || [];
 var elem = document.body;
 
+
+// Request Function For Fullscreen Function
 function requestFullScreen(element) {
   if (
     !document.fullscreenElement && // alternative standard method
@@ -34,6 +37,7 @@ function requestFullScreen(element) {
   }
 }
 
+// Fullscreen Function
 function toggleFullScreenButton() {
   requestFullScreen(elem);
 }
@@ -66,12 +70,14 @@ function turnOff() {
   }
 }
 
+// Save data to local storage function
 function saveCamerasToLocalStorage() {
   const cameras = Array.from(mainContent.querySelectorAll(".camera")).map(
     (page) => page.textContent
   );
   localStorage.setItem("cameras", JSON.stringify(cameras));
 }
+
 
 function allowDrop(event) {
   event.preventDefault();
@@ -92,18 +98,22 @@ function drop(event) {
   const page = document.createElement("div");
   const name = document.createElement("div");
   const icon = document.createElement("i");
-  const btn = document.createElement("i");
+  const windowCloseButton = document.createElement("i");
+  const windowFullButton = document.createElement("i");
   const foto = document.createElement("img");
   const item = document.createElement("li");
 
   page.className = "camera";
   icon.className = "fa-solid fa-video";
-  btn.className = "fa-solid fa-trash";
+  windowCloseButton.className = "fa-solid fa-square-xmark";
+  windowFullButton.className = "fa-solid fa-window-restore";
   foto.src = "/images/screen.png";
 
   icon.style = "color: green;";
-  btn.style = "color: red; margin-left:10px;";
-  btn.style.cursor = "pointer";
+  windowCloseButton.style = "color: red; position:absolute; margin-left:-20px; margin-top:8px; padding-right:1px; padding-left:1px; border-radius:3px;";
+  windowCloseButton.style.cursor = "pointer";
+  windowFullButton.style = "color: white; background-color:transparent; position:absolute; margin-left:-45px; margin-top:8px; padding-right:1px; padding-left:1px; border-radius:3px;";
+  windowFullButton.style.cursor = "pointer";
   name.className = "name";
   foto.className = "foto";
 
@@ -111,52 +121,98 @@ function drop(event) {
 
   item.insertBefore(icon, item.firstChild);
   list.appendChild(item);
-  item.appendChild(btn);
   page.appendChild(name);
   page.appendChild(foto);
+  page.appendChild(windowCloseButton);
+  page.appendChild(windowFullButton);
   mainContent.appendChild(page);
 
-  btn.addEventListener("click", () => {
+
+  windowCloseButton.addEventListener("click", () => {
     page.remove();
     list.removeChild(item);
     saveCamerasToLocalStorage();
   });
+
+  windowFullButton.addEventListener("click", () => {
+    if (page.classList.contains("fullScreen")) {
+      page.classList.remove("fullScreen");
+      page.style = ""; // Remove inline styles
+      foto.style = ""; // Remove inline styles
+      name.style = ""; // Remove inline styles
+      dropZone.style = ""; // Remove inline styles
+    } else {
+      page.classList.add("fullScreen");
+      page.style = "width:1300px;height:873px;margin-left: 100px; position:absolute; z-index:20;";
+      foto.style = "width: 100%;";
+      name.style = "width: 98.7%;";
+      dropZone.style = "background-color:white; z-index:19;"
+    }
+  });
+
+
   saveCamerasToLocalStorage();
 }
+
+// Add event listener for each dragged camera
+pages.forEach((page) => {
+  page.addEventListener("dragstart", function (event) {
+    event.dataTransfer.setData("text/plain", page.textContent);
+  });
+});
 
 // Restore previous session cameras on page load
 savedCameras.forEach((camera) => {
   const page = document.createElement("div");
   const name = document.createElement("div");
   const icon = document.createElement("i");
-  const btn = document.createElement("i");
+  const windowCloseButton = document.createElement("i");
+  const windowFullButton = document.createElement("i");
   const item = document.createElement("li");
   const foto = document.createElement("img");
 
   page.className = "camera";
   icon.className = "fa-solid fa-video green";
-  btn.className = "fa-solid fa-trash btn";
+  windowCloseButton.className = "fa-solid fa-square-xmark";
+  windowCloseButton.style = "color: red; position:absolute; margin-left:-20px; margin-top:8px; padding-right:1px; padding-left:1px; border-radius:3px;";
+  windowCloseButton.style.cursor = "pointer";
+  windowFullButton.className = "fa-solid fa-window-restore";
+  windowFullButton.style = "color: white; background-color:transparent; position:absolute; margin-left:-45px; margin-top:8px; padding-right:1px; padding-left:1px; border-radius:3px;";
+  windowFullButton.style.cursor = "pointer";
   foto.src = "/images/screen.png";
   foto.className = "foto";
   name.className = "name";
   item.textContent = name.innerText = camera;
-
   item.insertBefore(icon, item.firstChild);
   list.appendChild(item);
-  item.appendChild(btn);
   page.appendChild(name);
   page.appendChild(foto);
+  page.appendChild(windowFullButton);
+  page.appendChild(windowCloseButton);
   mainContent.appendChild(page);
 
-  btn.addEventListener("click", () => {
+  
+
+  windowCloseButton.addEventListener("click", () => {
     page.remove();
     list.removeChild(item);
     saveCamerasToLocalStorage(); // Update local storage after removing
   });
-});
 
-pages.forEach((page) => {
-  page.addEventListener("dragstart", function (event) {
-    event.dataTransfer.setData("text/plain", page.textContent);
+  windowFullButton.addEventListener("click", () => {
+    if (page.classList.contains("fullScreen")) {
+      page.classList.remove("fullScreen");
+      page.style = ""; // Remove inline styles
+      foto.style = ""; // Remove inline styles
+      name.style = ""; // Remove inline styles
+      dropZone.style = ""; // Remove inline styles
+    } else {
+      page.classList.add("fullScreen");
+      page.style = "width:1300px;height:873px;margin-left: 100px; position:absolute; z-index:20;";
+      foto.style = "width: 100%;";
+      name.style = "width: 98.7%;";
+      dropZone.style = "background-color:white; z-index:19;"
+    }
   });
+  
 });
